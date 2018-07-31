4ed792aa9b94d951702f8c33183ff8b81aa0fd61
armstrom02 <viveksinghreborn@gmail.com>
2018-07-31 09:25:40 +0530

    third commit

--- app.component.ts
+++ app.component.ts
@@ -12,14 +12,14 @@ import { FormBuilder } from '@angular/forms';
 export class AppComponent {
   title = 'FormReactive';
   profileForm = new FormGroup({
-    firstName: new FormControl('',[Validators.required]),
-    lastName: new FormControl('',[Validators.required]),
-    email: new FormControl('',[Validators.required,Validators.email]),
-    contactno: new FormControl('',[Validators.required]),
-    password: new FormControl('',[Validators.required]),
-    confirmPassword: new FormControl('',[Validators.required]),
-    gender: new FormControl('',[Validators.required]),
-    EmpId: new FormControl('',[Validators.required]),
+    firstName: new FormControl('',[Validators.required,Validators.maxLength(15),Validators.pattern("^[a-zA-Z]*$")]),
+    lastName: new FormControl('',[Validators.required,Validators.maxLength(15),Validators.pattern("^[a-zA-Z]*$")]),
+    email: new FormControl('',[Validators.required,Validators.email,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$")]),
+    contactno: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(10),Validators.pattern('[0-9]*')]),
+    password: new FormControl('',[Validators.required,Validators.pattern("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/")]),
+    confirmPassword: new FormControl('',[Validators.required,Validators.pattern("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/")]),
+    gender: new FormControl('',[Validators.required,Validators.maxLength(15),Validators.pattern("^[a-zA-Z]*$")]),
+    EmpId: new FormControl('',[Validators.required,Validators.maxLength(4),Validators.minLength(4),Validators.pattern('[0-9]*')]),
   });
   
   onSubmit(){
--- app.module.ts
+++ app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import{FormsModule} from'@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
